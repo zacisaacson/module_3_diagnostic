5484a2588842b322c50ae5d3f0b7c2e11a1ff70d
Zac Isaacson <isaacson.zm@gmail.com>
2019-12-09 11:03:21 -0700

    Add render to controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,9 @@
 class SearchController < ApplicationController
   def index
-    house = params[:house]
 
-    conn = Faraday.new(url: "https://www.potterapi.com") do |faraday|
-      faraday.headers["key"] = "$2a$10$s.JfA/89briDCP8Fowq7Oe8WZqVctmVlnVisJIMEk9CgNcxCR7AyO"
-      faraday.adapter Faraday.default_adapter
-
-    end
-    response = conn.get("/v1/characters?key=#{ENV["POTTER_API_KEY"]}&house=#{house}&orderOfThePhoenix=true")
-
-    @members = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+    render locals: {
+      search_results: PhoenixMemberSearch.new(params[:house])
+    }
   end
+
 end
