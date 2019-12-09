4dafadc383cb248e626c10e8d1fdc9ae6da1746e
Zac Isaacson <isaacson.zm@gmail.com>
2019-12-09 11:03:57 -0700

    Create member search PORO

---
+++ app/poros/phoenix_member_search.rb
@@ -0,0 +1,21 @@
+class PhoenixMemberSearch
+
+  def initialize(house)
+    @house = house
+  end
+
+  def member_count
+    members.count
+  end
+
+  def members
+    service = PotterService.new
+    service.members_by_house(house).map do |member_data|
+      Member.new(member_data)
+    end
+  end
+  
+  private
+
+  attr_reader :house
+end
