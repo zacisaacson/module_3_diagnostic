dc694c5947570abc962870e3cd1acd8a93a6e4c2
Zac Isaacson <isaacson.zm@gmail.com>
2019-12-09 11:27:35 -0700

    Refactor and seperate members by house method into private methods

--- app/services/potter_service.rb
+++ app/services/potter_service.rb
@@ -1,11 +1,18 @@
 class PotterService
   def members_by_house(house)
-    conn = Faraday.new(url: "https://www.potterapi.com") do |faraday|
+    get_json("/v1/characters?key=#{ENV["POTTER_API_KEY"]}&house=#{house}&orderOfThePhoenix=true")
+  end
+
+  private
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://www.potterapi.com") do |faraday|
       faraday.adapter Faraday.default_adapter
     end
-
-    response = conn.get("/v1/characters?key=#{ENV["POTTER_API_KEY"]}&house=#{house}&orderOfThePhoenix=true")
-
-    member_search_data = JSON.parse(response.body, symbolize_names: true)
   end
 end
